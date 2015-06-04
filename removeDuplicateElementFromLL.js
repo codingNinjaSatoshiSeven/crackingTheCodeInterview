f86ab46f887259ff1167b3fb301fd736bf65c8b7
Seven Li <sevenlist0110@gmail.com>
2015-06-04 10:13:02 +0800

    [doc] write the questions prompt for removeDuplicateElementFromLL

--- removeDuplicateElementFromLL.js
+++ removeDuplicateElementFromLL.js
@@ -1,3 +1,8 @@
+/** Prompt:
+* Write code to remove duplicates from an unsorted linked list. 
+* follow up: how would you  solve this problem if a temporary buffer is not allowed?
+*/
+
 var LinkedList = function(){
   var list = {};
   list.head = null;
